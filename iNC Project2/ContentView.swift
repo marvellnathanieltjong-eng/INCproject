a168fa0f128c0a50ef7cbbcd1aeba45cdb131c54
Marvell <marvell_nathaniel_tjong@s2026.ssts.edu.sg>
2026-09-03 20:23:23 +0800

    First page of app

--- iNC Project2/ContentView.swift
+++ iNC Project2/ContentView.swift
@@ -8,17 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var math = false
+    @State var value = 0
+    @State var science = false
+    @State var value2 = 0
+    @State var value3 = 0
+    @State var english = false
+    @State var chinese = false
+    @State var value4 = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            Text("List of Homework")
+                .font(Font.system(size: 50))
+                .bold()
+                .foregroundStyle(.primary)
+            
+            Toggle("1. Math Homework", isOn: $math)
+                .font(Font.system(size: 25))
+                .foregroundStyle(Color.blue)
+            if math {
+                Text("Great Job! Math Homework is done!")
+            }
+            
+           Stepper("Quantity: \(value)", value: $value, step: 1)
+            
+            Toggle("2. Science Homework", isOn: $science )
+                .font(Font.system(size: 25))
+                .foregroundStyle(Color.blue)
+            if science {
+                Text("Congrats! You rocked for your Science Homework!")
+            }
+            
+            Stepper("Quantity: \(value2)", value: $value2, step: 1)
+            Toggle("3. English Homework", isOn: $english)
+                .font(Font.system(size: 25))
+                .foregroundStyle(Color.blue)
+            if english {
+                Text("English homework completed!")
+            }
+            Stepper("Quantity: \(value3)", value: $value3, step: 1)
+            Toggle("4. Chinese Homework", isOn: $chinese)
+                .font(Font.system(size: 25))
+                .foregroundStyle(Color.blue)
+            if chinese {
+                Text("Amazing job! Continue practising your Chinese!")
+            }
+            Stepper("Quantity: \(value4)", value: $value4, step: 1)
+        
+            Spacer()
+                .padding()
+            }
         .padding()
+        }
+    
+    
     }
-}
+
+
 
 #Preview {
     ContentView()
 }
+
